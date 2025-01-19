4689da83d02e57a7233dde9ba25518e759448fe0
PauloGerson <paulogerson81@gmail.com>
2025-01-19 12:42:19 -0300

    Lista de Convidados.

---
+++ app/src/main/java/devandroid/paulo/applistacurso/MainActivity.java
@@ -0,0 +1,36 @@
+package devandroid.paulo.applistacurso;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
+    TextView txt1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txt1 = findViewById(R.id.txt);
+        btn1 = findViewById(R.id.btn);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txt1.setText("Bora programar");
+            }
+        });
+    }
+}
