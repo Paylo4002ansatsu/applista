ea0179cabf1d87a73a340233d8be882fd8d46428
PauloGerson <paulogerson81@gmail.com>
2025-01-20 21:49:25 -0300

    Criação do layout

--- app/src/main/java/devandroid/paulo/applistacurso/MainActivity.java
+++ app/src/main/java/devandroid/paulo/applistacurso/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txt1 = findViewById(R.id.txt);
+
 
 
 
